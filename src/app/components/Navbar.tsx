0d33b3754a928ec9cdc7a733e0cd252844d707f0
IlsaTabassum <160242392+IlsaTabassum@users.noreply.github.com>
2024-10-29 21:37:36 +0500

    Add files via upload

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+export default function Navbar(){
+    return(
+<header className="bg-black text-white">
+<div className="flex justify-between items-center p-1 text-sm bg-gray-900 text-gray-300"> 
+    <div className="flex-items-center">
+        <span className="bg-red-500 mr-2"></span>
+        <span> Download app via SMS</span>
+    </div>
+    <div>
+        <span className="mr-4">Sign Up</span> | <span className="mr-4">Sign In</span>
+    </div>
+</div>
+<div className="container mx-auto flex"
+</header>
+    )
+}
